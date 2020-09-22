10ef153fab78b4719100d2acd9a78f1fcb92ad43
andresbarros23 <andrees_2011@yahoo.com.ar>
2020-09-22 16:47:38 -0300

    Adding reports

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -10,7 +10,7 @@ import org.junit.runner.RunWith;
         glue = {"steps", "hooks"},
         tags = "@Regression or @Smoke",
         dryRun = false, //This validates that all the steps defined have their implementation. False by default.
-        monochrome = false, //The report log is formatted and cleaner.
+        monochrome = false, //The report log is formatted and cleaner. False by default.
         plugin = {"pretty", "html:target/cucumber-reports/report.html",
                 "json:target/cucumber-reports/report.json",
                 "junit:target/cucumber-reports/cukes.xml"}
